54a4141493fef2da648c883679ba4439b19ec1dd
Shalmali <shalmalibichkar@gmail.com>
2025-07-11 22:58:55 +0530

    07reactROuter-complete

---
+++ mini-projects/07reactRouter/src/components/Github.jsx
@@ -0,0 +1,31 @@
+import React, {useEffect} from 'react'
+import { useState } from 'react'
+import {useLoaderData} from 'react-router-dom'
+
+function Github(){
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(()=>{
+    //     fetch('https://api.github.com/users/hiteshchoudhary')
+    //     .then(response => response.json())
+    //     .then(data =>{
+    //         console.log(data)
+    //         setData(data)
+    //     }
+    //     )
+    // },[])
+
+    return(
+        <div className = "bg-gray-600 text-white text-3xl p-4 text-center"> Github followers:{data.followers}
+        <img src = {data.avatar_url} alt = "Git picture" width = {300}/>
+        </div>
+        
+    )
+}
+
+export default Github
+
+export const githubInfoLoader = async()=> {
+    const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+    return response.json()
+}
