89c5e16002b1f76f5fc9f8d7ad66af258f193199
Shalmali <shalmalibichkar@gmail.com>
2025-07-14 07:01:08 +0530

    todo completed

---
+++ todoListContextLocal/src/contexts/TodoContext.js
@@ -0,0 +1,23 @@
+import React,{createContext, useContext} from 'react'
+
+export const TodoContext = createContext({
+    todos:[
+        {
+            id:1,
+            todo:"Todo message",
+            completed:false,
+        }
+    ],
+    addTodo: (todo) =>{},
+    updateTodo: (id,todo) =>{},
+    deleteTodo: (id) =>{},
+    toggleComplete: (id) =>{},
+})
+
+export const TodoProvider = TodoContext.Provider
+
+export const useTodo =()=>{
+    return(
+        useContext(TodoContext)
+    )
+} 
